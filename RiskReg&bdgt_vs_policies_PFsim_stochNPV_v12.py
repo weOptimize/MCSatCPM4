f162640ff3394d30ab74ea68978e7e5d97dadc9d
Miguel <msaiz@weoptimize.net>
2023-05-18 23:00:26 +0200

    modified:   RiskReg&bdgt_vs_policies_PFsim_stochNPV_v12.py 	modified:   functions_for_simheuristic_v12.py     Copula and beta distribution moved to file functions_for_simheuristic_v12

--- RiskReg&bdgt_vs_policies_PFsim_stochNPV_v12.py
+++ RiskReg&bdgt_vs_policies_PFsim_stochNPV_v12.py
@@ -9,9 +9,9 @@ from pandas_ods_reader import read_ods
 from operator import itemgetter
 import matplotlib.pyplot as plt 
 from scipy import stats as st
-from copulas.multivariate import GaussianMultivariate
-from scipy.stats import rv_continuous, rv_histogram, norm, uniform, multivariate_normal, beta
-from fitter import Fitter, get_common_distributions, get_distributions
+#from copulas.multivariate import GaussianMultivariate
+#from scipy.stats import rv_continuous, rv_histogram, norm, uniform, multivariate_normal, beta
+#from fitter import Fitter, get_common_distributions, get_distributions
 
 #import created scripts:
 from task_rnd_triang_with_interrupts_stdev_new_R2 import *
@@ -77,131 +77,8 @@ totalcost = np.sum(x_perproj_matrix1[0])
 print("total portfolio cost allocation request:")
 print(totalcost)
 
+df10r = correlatedMCS(mcs_results1, iterations)
 
-
-
-#check the parameters of beta distribution for each of the mcs_results
-betaparams = []
-for i in range(nrcandidates):
-    f = Fitter(mcs_results1[0][i], distributions=['beta'])
-    f.fit()
-    betaparam=(f.fitted_param["beta"])
-    betaparams.append(betaparam)
-
-#extract all "a" parameters from the betaparams array
-a = []
-for i in range(nrcandidates):
-    a.append(betaparams[i][0])
-
-#extract all "b" parameters from the betaparams array
-b = []
-for i in range(nrcandidates):
-    b.append(betaparams[i][1])
-
-#extract all "loc" parameters from the betaparams array
-loc = []
-for i in range(nrcandidates):
-    loc.append(betaparams[i][2])
-
-#extract all "scale" parameters from the betaparams array
-scale = []
-for i in range(nrcandidates):
-    scale.append(betaparams[i][3])
-
-
-print(betaparams)
-
-# copy the array with all MCS results
-df0 = pd.DataFrame(data=mcs_results1[0]).T
-df0.rename(columns={0:"P01", 1:"P02", 2:"P03", 3:"P04", 4:"P05", 5:"P06", 6:"P07", 7:"P08", 8:"P09", 9:"P10"}, inplace=True)
-correlation_matrix0 = df0.corr()
-
-# *********Correlation matrix with random values between 0 and 1, but positive semidefinite***************
-# Set the seed value for the random number generator  
-seed_value = 1005  
-np.random.seed(seed_value)
-# Generate a random symmetric matrix
-A = np.random.rand(10, 10)
-A = (A + A.T) / 2
-# Compute the eigenvalues and eigenvectors of the matrix
-eigenvalues, eigenvectors = np.linalg.eigh(A)
-# Ensure the eigenvalues are positive
-eigenvalues = np.abs(eigenvalues)
-# Normalize the eigenvalues so that their sum is equal to 10
-eigenvalues = eigenvalues / eigenvalues.sum() * 10
-# Compute the covariance matrix. Forcing positive values, as long as negative correlations are not usual in reality of projects
-cm10r = np.abs(eigenvectors.dot(np.diag(eigenvalues)).dot(eigenvectors.T))
-# Ensure the diagonals are equal to 1
-for i in range(10):
-    cm10r[i, i] = 1
-print('cm10r:')
-print(cm10r)
-
-#initialize dataframe df10r with size nrcandidates x iterations
-df10r = pd.DataFrame(np.zeros((iterations, nrcandidates)))
-# step 1: draw random variates from a multivariate normal distribution 
-# with the targeted correlation structure
-r0 = [0] * cm10r.shape[0]                       # create vector r with as many zeros as correlation matrix has variables (row or columns)
-mv_norm = multivariate_normal(mean=r0, cov=cm10r)    # means = vector of zeros; cov = targeted corr matrix
-rand_Nmv = mv_norm.rvs(iterations)                               # draw N random variates
-# step 2: convert the r * N multivariate variates to scores 
-rand_U = norm.cdf(rand_Nmv)   # use its cdf to generate N scores (probabilities between 0 and 1) from the multinormal random variates
-# step 3: instantiate the 10 marginal distributions 
-d_P1 = beta(a[0], b[0], loc[0], scale[0])
-d_P2 = beta(a[1], b[1], loc[1], scale[1])
-d_P3 = beta(a[2], b[2], loc[2], scale[2])
-d_P4 = beta(a[3], b[3], loc[3], scale[3])
-d_P5 = beta(a[4], b[4], loc[4], scale[4])
-d_P6 = beta(a[5], b[5], loc[5], scale[5])
-d_P7 = beta(a[6], b[6], loc[6], scale[6])
-d_P8 = beta(a[7], b[7], loc[7], scale[7])
-d_P9 = beta(a[8], b[8], loc[8], scale[8])
-d_P10 = beta(a[9], b[9], loc[9], scale[9])
-# draw N random variates for each of the three marginal distributions
-# WITHOUT applying a copula
-rand_P1 = d_P1.rvs(iterations)
-rand_P2 = d_P2.rvs(iterations)
-rand_P3 = d_P3.rvs(iterations)
-rand_P4 = d_P4.rvs(iterations)
-rand_P5 = d_P5.rvs(iterations)
-rand_P6 = d_P6.rvs(iterations)
-rand_P7 = d_P7.rvs(iterations)
-rand_P8 = d_P8.rvs(iterations)
-rand_P9 = d_P9.rvs(iterations)
-rand_P10 = d_P10.rvs(iterations)
-# initial correlation structure before applying a copula
-c_before = np.corrcoef([rand_P1, rand_P2, rand_P3, rand_P4, rand_P5, rand_P6, rand_P7, rand_P8, rand_P9, rand_P10])
-# step 4: draw N random variates for each of the three marginal distributions
-# and use as inputs the correlated uniform scores we have generated in step 2
-rand_P1 = d_P1.ppf(rand_U[:, 0])
-rand_P2 = d_P2.ppf(rand_U[:, 1])
-rand_P3 = d_P3.ppf(rand_U[:, 2])
-rand_P4 = d_P4.ppf(rand_U[:, 3])
-rand_P5 = d_P5.ppf(rand_U[:, 4])
-rand_P6 = d_P6.ppf(rand_U[:, 5])
-rand_P7 = d_P7.ppf(rand_U[:, 6])
-rand_P8 = d_P8.ppf(rand_U[:, 7])
-rand_P9 = d_P9.ppf(rand_U[:, 8])
-rand_P10 = d_P10.ppf(rand_U[:, 9])
-# final correlation structure after applying a copula
-c_after = np.corrcoef([rand_P1, rand_P2, rand_P3, rand_P4, rand_P5, rand_P6, rand_P7, rand_P8, rand_P9, rand_P10])
-print("Correlation matrix before applying a copula:")
-print(c_before)
-print("Correlation matrix after applying a copula:")
-print(c_after)
-# step 5: store the N random variates in the dataframe
-df10r[0] = rand_P1
-df10r[1] = rand_P2
-df10r[2] = rand_P3
-df10r[3] = rand_P4
-df10r[4] = rand_P5
-df10r[5] = rand_P6
-df10r[6] = rand_P7
-df10r[7] = rand_P8
-df10r[8] = rand_P9
-df10r[9] = rand_P10
-df10r.rename(columns={0:"P01", 1:"P02", 2:"P03", 3:"P04", 4:"P05", 5:"P06", 6:"P07", 7:"P08", 8:"P09", 9:"P10"}, inplace=True)
-correlation_matrix1 = df10r.corr()
 
 # Defining the fitness function
 def evaluate(individual, bdgtperproject, npvperproject, maxbdgt):
@@ -445,7 +322,7 @@ plt.tight_layout()
 #extract the sixth portfolio included in array portfolio_results (RESTORE TO SIX WHEN MORE THAN ONE BCP!!!!!!!!!!!!!)
 chosen_portfolio = portfolio_results[0]
 #multiply dataframe 0 by the chosen portfolio to reflect the effect of the projects that are chosen
-pf_df = df0 * chosen_portfolio
+pf_df = df10r * chosen_portfolio # was pf_df = df0 * chosen_portfolio
 #sum the rows of the new dataframe to calculate the total cost of the portfolio
 pf_cost = pf_df.sum(axis=1)
 
